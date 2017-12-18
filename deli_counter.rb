8b2eb3d06ad0e84850917233f0a2e08d3fb2374c
Onur Eker <yonureker@gmail.com>
2017-12-18 23:53:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push("#{name}")
-  "Welcome #{name}, you are number #{katz_deli.length} in line."
+  "Welcome #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
