1d1b18454d61a9050e3f0eb637df71552896153a
Onur Eker <yonureker@gmail.com>
2017-12-18 23:41:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,11 @@ def line(katz_deli)
   if katz_deli.length == 0
     "The line is currently empty."
   else
-    text = "The line is currently: "
-    katz_deli.each do |name|
-      text << |name|
+    text = "The line is currently:"
+    katz_deli.each.with_index do |name, index|
+      text << " #{index + 1}. #{name}"
+    end
+    return text
   end
 end
 
