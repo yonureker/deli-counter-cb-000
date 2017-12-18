cd9970ddb1f075f2db362b49e6c1e94c4e78fe48
Onur Eker <yonureker@gmail.com>
2017-12-18 23:35:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    "The line is currently empty."
+  else
+    text = "The line is currently: "
+    katz_deli.each do |name|
+      text << |name|
+  end
+end
+
+def take_a_number(katz_deli, name)
+
+end
+
+def now_serving
+end
