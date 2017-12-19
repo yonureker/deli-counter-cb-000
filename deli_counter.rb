0bf0864fca85d8dbb17bc9e5c6bec4aae50c8e60
Onur Eker <yonureker@gmail.com>
2017-12-19 00:03:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,27 +21,7 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     "There is nobody waiting to be served!"
   else
-    return "Currently serving #{katz_deli[0]}."
+    "Currently serving #{katz_deli[0]}."
     katz_deli.shift
   end
 end
-
-katz_deli = []
-
-take_a_number(katz_deli, "Ada") #=> Welcome, Ada. You are number 1 in line.
-take_a_number(katz_deli, "Grace") #=> Welcome, Grace. You are number 2 in line.
-take_a_number(katz_deli, "Kent") #=> Welcome, Kent. You are number 3 in line.
-
-line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
-
-now_serving(katz_deli) #=> "Currently serving Ada."
-
-line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
-
-take_a_number(katz_deli, "Matz") #=> Welcome, Matz. You are number 3 in line.
-
-line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent 3. Matz"
-
-now_serving(katz_deli) #=> "Currently serving Grace."
-
-line(katz_deli)
